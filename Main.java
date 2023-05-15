aa6454ef2eec955ec12579d8a46361072349c440
Rahul R <Rahul.R3@aexp.com>
2023-05-15 14:46:13 +0530

    Add useless code

--- Main.java
+++ Main.java
@@ -6,8 +6,15 @@ public abstract class Main {
         // System.out.println("Hello, World");
         // System.out.println(foo(0)); // -> 1
         // System.out.println(32768);
+
+        // System.out.println(1.0f);
+        // System.out.println(0.0f);
+        // System.out.println(2.0f);
         System.out.println(1.234f);
         System.out.println(42f);
+
+        // py: int('111', 2) << 8 | int('11111111', 2)
+
         System.out.println(1234549828982398293L);
         // System.out.println(0L);
         // System.out.println();
